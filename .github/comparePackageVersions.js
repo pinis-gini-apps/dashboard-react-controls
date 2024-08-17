d92eaf3e1c976a60532db989e0cbcb6be19d5ba1
pinis-gini-apps <pinis@gini-apps.com>
2024-08-17 20:02:07 +0300

    update ci workflow

--- .github/comparePackageVersions.js
+++ .github/comparePackageVersions.js
@@ -16,37 +16,36 @@ exec(command, (error, stdout, stderr) => {
 
   console.log(`stdout:\n${stdout}`)
 })
-// const packageJsonPath = path.join(__dirname, 'package.json')
-// console.log('------1---------')
-// console.log(packageJsonPath)
-// const getVersionFromPackageJson = branch => {
-//   try {
-//     execSync(`git checkout ${branch} -- ${packageJsonPath}`)
-//     const data = fs.readFileSync(packageJsonPath, 'utf8')
-//     console.log('------2---------')
-//     console.log(data)
-//     const packageJson = JSON.parse(data)
-//     return packageJson.version
-//   } catch (err) {
-//     console.error(`Error reading package.json from branch ${branch}:`, err)
-//     process.exit(1)
-//   }
-// }
-//
-// const targetBranch = process.env.TARGET_BRANCH || 'development'
-// console.log('------3---------')
-//
-// console.log(targetBranch)
-// const currentVersion = getVersionFromPackageJson('HEAD')
-//
-// const targetVersion = getVersionFromPackageJson(targetBranch)
-//
-// if (currentVersion !== targetVersion) {
-//   console.error(
-//     `Version mismatch: current branch (${currentVersion}), target branch (${targetVersion})`
-//   )
-//   process.exit(1)
-// } else {
-//   console.log(`Version is correct: ${currentVersion}`)
-//   process.exit(0)
-// }
+const packageJsonPath = path.join(__dirname, 'package.json')
+const getVersionFromPackageJson = branch => {
+  try {
+    execSync('git status')
+    execSync(`git checkout ${branch} -- package.json`)
+    const data = fs.readFileSync('package.json', 'utf8')
+    console.log('------2---------')
+    console.log(data)
+    const packageJson = JSON.parse(data)
+    return packageJson.version
+  } catch (err) {
+    console.error(`Error reading package.json from branch ${branch}:`, err)
+    process.exit(1)
+  }
+}
+
+const targetBranch = process.env.TARGET_BRANCH || 'development'
+console.log('------3---------')
+
+console.log(targetBranch)
+const currentVersion = getVersionFromPackageJson('HEAD')
+
+const targetVersion = getVersionFromPackageJson(targetBranch)
+
+if (currentVersion !== targetVersion) {
+  console.error(
+    `Version mismatch: current branch (${currentVersion}), target branch (${targetVersion})`
+  )
+  process.exit(1)
+} else {
+  console.log(`Version is correct: ${currentVersion}`)
+  process.exit(0)
+}
