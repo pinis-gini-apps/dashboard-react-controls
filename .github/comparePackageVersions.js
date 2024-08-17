7e18f689d92270c3d657360daa7607a80b1ac665
pinis-gini-apps <pinis@gini-apps.com>
2024-08-17 23:31:49 +0300

    change version for testing

--- .github/comparePackageVersions.js
+++ .github/comparePackageVersions.js
@@ -27,10 +27,10 @@ console.log(currentBranch)
 const getVersionFromBranch = branch => {
   try {
     console.log('----git branch -a-----')
-    const branchList = execSync('git branch -a')
+    const branchList = execSync('git branch -a', { encoding: 'utf-8' })
     console.log(branchList)
     console.log('----git branch-----')
-    const branchList2 = execSync('git branch')
+    const branchList2 = execSync('git branch', { encoding: 'utf-8' })
     console.log(branchList2)
     execSync(`git checkout ${branch} -- ${packageJsonPath}`)
     const data = fs.readFileSync(packageJsonPath, 'utf8')
